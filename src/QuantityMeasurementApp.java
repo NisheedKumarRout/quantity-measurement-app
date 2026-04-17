8670b997132a9192f2006061a8229a33189b7cf2
NisheedKumarRout <nisheedkumarrout@gmail.com>
2026-04-17 16:29:34 +0530

    UC2: Feet and Inches equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,6 +1,50 @@
 public class QuantityMeasurementApp {
 
-    public boolean quantityMeasurementApp(double value1, double value2) {
-        return value1 == value2;
+    // Method for Feet equality
+    public boolean quantityMeasurementApp(double value1, double value2, String unit) {
+        if (unit.equalsIgnoreCase("feet")) {
+            Feet f1 = new Feet(value1);
+            Feet f2 = new Feet(value2);
+            return f1.equals(f2);
+        } else if (unit.equalsIgnoreCase("inches")) {
+            Inches i1 = new Inches(value1);
+            Inches i2 = new Inches(value2);
+            return i1.equals(i2);
+        }
+        return false;
+    }
+
+    // Inner class for Feet
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
+        }
+    }
+
+    // Inner class for Inches
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, value) == 0;
+        }
     }
 }
--- src/test/java/QuantityMeasurementAppTest.java
+++ src/test/java/QuantityMeasurementAppTest.java
@@ -6,24 +6,24 @@ class QuantityMeasurementAppTest {
     @Test
     void givenSameFeetValues_shouldReturnTrue() {
         QuantityMeasurementApp app = new QuantityMeasurementApp();
-        assertTrue(app.quantityMeasurementApp(5.0, 5.0));
+        assertTrue(app.quantityMeasurementApp(5.0, 5.0, "feet"));
     }
 
     @Test
     void givenDifferentFeetValues_shouldReturnFalse() {
         QuantityMeasurementApp app = new QuantityMeasurementApp();
-        assertFalse(app.quantityMeasurementApp(5.0, 6.0));
+        assertFalse(app.quantityMeasurementApp(5.0, 6.0, "feet"));
     }
 
     @Test
-    void givenZeroValues_shouldReturnTrue() {
+    void givenSameInchesValues_shouldReturnTrue() {
         QuantityMeasurementApp app = new QuantityMeasurementApp();
-        assertTrue(app.quantityMeasurementApp(0.0, 0.0));
+        assertTrue(app.quantityMeasurementApp(10.0, 10.0, "inches"));
     }
 
     @Test
-    void givenNegativeValues_shouldReturnTrue() {
+    void givenDifferentInchesValues_shouldReturnFalse() {
         QuantityMeasurementApp app = new QuantityMeasurementApp();
-        assertTrue(app.quantityMeasurementApp(-2.0, -2.0));
+        assertFalse(app.quantityMeasurementApp(10.0, 12.0, "inches"));
     }
 }
